d5de5f588dec864e30626102065b3c4e86724c46
Igor Popov <igor@igorpopov.io>
2015-04-24 20:07:30 +0300

    Extract class for methods used in tests
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- src/test/java/com/gildedrose/LessRestrictiveItemContractTests.java
+++ src/test/java/com/gildedrose/LessRestrictiveItemContractTests.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-public abstract class LessRestrictiveItemContractTests {
+public abstract class LessRestrictiveItemContractTests extends ItemBaseTests {
     @Test
     public void qualityCannotBeNegativeEvenAfterExpiration() throws Exception {
         Item item = createItemWithSellInAndQuality(0, 0);
@@ -24,7 +24,6 @@ public abstract class LessRestrictiveItemContractTests {
         assertTrue(item.quality <= 50);
     }
 
-
     @Test
     @Ignore("TODO: Should fail, but doesn't. Fix later!")
     public void cannotBeCreatedWithNegativeQuality() {
@@ -35,28 +34,5 @@ public abstract class LessRestrictiveItemContractTests {
     @Ignore("TODO: Should fail, but doesn't. Fix later!")
     public void cannotBeCreatedWithQualityMoreThan50() throws Exception {
         createItemWithSellInAndQuality(10, 51);
-    }
-
-    public Item createItemWithSellIn(int sellIn) {
-        return new Item(getDefaultItemName(), sellIn, getDefaultItemQuality());
-    }
-
-
-    public Item createItemWithSellInAndQuality(int sellIn, int quality) {
-        return new Item(getDefaultItemName(), sellIn, quality);
-    }
-
-    public int getDefaultItemQuality() {
-        return 10;
-    }
-
-
-    public abstract String getDefaultItemName();
-
-    public Item updateQualityFor(Item item) {
-        Item[] items = new Item[]{item};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        return item;
     }
 }
